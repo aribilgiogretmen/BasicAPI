92deda112242b4410a057157ee267db23308eb31
Arı Bilgi <aribilgiogretmen@gmail.com>
2024-07-20 11:15:50 +0300

    Add project files.

---
+++ BasicAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BasicAPI.Models;
+
+namespace BasicAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+
+        public DbSet<Product> Product { get; set; }
+       
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            //modelBuilder.Entity<Job>(e => e.Property(a => a.Aciklama).HasColumnName("Description"));
+        }
+
+
+    }
+}
